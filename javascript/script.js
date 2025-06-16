597b5fa76aaf5b013d8866d1fb17afb090688f79
sowndharya sri <sowndharyasri228@gmail.com>
2025-06-16 15:19:34 +0530

    created

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+var a="10";
+var b=20;
+console.log(a*b);
+
+console.log(typeof a);
+console.log(typeof b);
+a=true;
+console.log(typeof a);
+var c;
+console.log(typeof c);
+var c=null;
+console.log(typeof c);
